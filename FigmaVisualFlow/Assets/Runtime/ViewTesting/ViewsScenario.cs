25d3f7d22e9f36f6d3fd1e0500e805c89bc1eecd
Mefodei <mefodei.link@gmail.com>
2023-05-24 02:28:27 +0400

    - update view scenario

---
+++ FigmaVisualFlow/Assets/Runtime/ViewTesting/ViewsScenario.cs
@@ -0,0 +1,32 @@
+using Cysharp.Threading.Tasks;
+using UniGame.Context.Runtime.Extension;
+using UniGame.ViewSystem.Runtime;
+using UniModules.GameFlow.Runtime.Core;
+using UnityEngine;
+
+namespace Runtime.ViewTesting
+{
+    using Views;
+
+    public class ViewsScenario : MonoBehaviour
+    {
+        public UniGraph graph;
+    
+        // Start is called before the first frame update
+        private void Start()
+        {
+            ExecuteAsync().Forget();
+        }
+
+        private async UniTask ExecuteAsync()
+        {
+            var context = graph.GraphContext;
+
+            var viewSystem = await context.ReceiveFirstAsync<IGameViewSystem>();
+        
+            Debug.Log("View System Initialized");
+
+            await viewSystem.OpenWindow<DemoEcsView>();
+        }
+    }
+}
